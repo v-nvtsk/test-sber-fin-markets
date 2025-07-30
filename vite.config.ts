ac8aad9b9505b2b232eaddcb452b0f5de466cb9c
v-nvtsk <v@nvtsk.ru>
2025-07-30 11:14:40 +0300

    fix(ci): change github-pages path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  define: { "import.meta.env.VITE_BASE_URL": JSON.stringify(
+    process.env.CI ? "/test-sber-fin-markets/" : "/",
+  ) },
+  base: process.env.CI ? "/test-sber-fin-markets/" : "/",
+  build: {
+    outDir: "dist",
+  },
+  plugins: [
+    react(),
+  ],
+  server: {
+    allowedHosts: true,
+  },
+});
